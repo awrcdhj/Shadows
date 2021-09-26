837b8d281f75fa86a3d0443259fb7bbd1d7217e0
awrcdhj <941877427@qq.com>
2021-09-26 14:21:39 +0800

    Update Dayone.js

--- Dayone.js
+++ Dayone.js
@@ -1,3 +1,15 @@
+var obj = JSON.parse($response.body);
+
+obj= {
+  "active_expires_at" : "2029-01-01T00:00:00Z",
+  "is_subscription_active" : true,
+  "active_subscription_type" : "premium",
+  "is_blocked" : false
+};
+
+$done({body: JSON.stringify(obj)});
+
+// Descriptions
 if ($response.status == 200) {
   if ($request.url.endsWith("account-status")) {
 $done({body: JSON.stringify({
