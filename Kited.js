68cf799a65b18c4756a17902f0d53c06deeeb26e
阿亮 <chenloveaesliang@qq.com>
2022-05-06 17:32:24 +0800

    Add files via upload

---
+++ Kited.js
@@ -0,0 +1,89 @@
+/* WebSocket Scripture  to load links(server ip please refer proxy group) Host Address see the name: (DNS.js )quota load */
+
+[Scriptwriter]
+
+#bin/bash
+
+#Scripture
+
+Kite1=rename:wap;
+Kite2=rename:net;
+Kite3=province:Hubei;
+Kite4=City:[HB];
+Suite7=fel;
+Renamek1=rip;
+Renamek2=road;
+Mime1=Kit2;
+Mime2=Kit2;
+M1mebe=er1;
+M1mebder=$;
+M2mebe=er2;
+
+#Scriper
+ - Ripe-Kited1-WAPPINGGT2$
+=like("$"){
+ image-Use
+ |SUITd7=mimebl("1$"){
+=felin,「
+ FELtamialpe=>("10.0.0/8")Port<'("80","443","8080")'>
+ass=It(j"$")\
+{The&linkDran'he-quot&("Child"){
+ -City&&T»Hl/Symsystem-GPSS=<('HubeiJzWuhand')>/TURE IPS\{}
+&at:System=>require,1$with,2$==require,impasse:$("113.240.72.195"),require,export:$("80", "443")= 1,2$){
+%/addressL=D$3,D$4&¢1&&£2$=tube
+}loadlye[HB]==&4("$")
+"$")}
+enabled-net=I(d-
+ >GpsedLsystems-l'ith$=wan
+-from_etc>***+Catl'it-quote>linkDeri&i4)&&("$")};
+ dong>Yunann=REJECT=yesE}
+{1$&2$=Hlb-
+"$"){regional //地区属性请参考：Proxy_Drama.js，脚本文件
+  }
+}
+#Scriper
+GPSIP/System/
+-Please>DIDeYunnann>ip&address=Shiele=itd<'change'>}
+}(LyHubel&atChin\$from_etc_system*/|[ing=("$")}
+besides&&der-set&l't-
+- vlsasat^place
+=("siten-namek=[H")-
+{</an&linkdra=H&&quot:("$")}
+City&T»system-GPSS-(Hlb)=> true
+<Script/></
+:{(/enabled-net=(Id){
+ GPSILsystemsOit$=>
+{/]&at-from_etc<*+Cantt&l'it$quot.linkder)[dong(Yunan]=REJECT
+</Script>do($)
+}$done()</Scripture>
+</Scriptd>$</Script
+<= dear/>
+[Hubelpd]
+119.96.0.0/ </Scripd>
+116.208.0.0/ </Scripd>
+111.172.0.0/ </Scripd>
+175.24.0.0/ </Scripd>
+125.220.0.0/ </Scripd>
+59.174.0.0/ </Scripd>
+119.36.0.0/ </Scripd>
+116.207.0.0/ </Scripd>
+203.176.0.0/ </Scripd>
+202.63.160.0/ </Scripd>
+182.239.0.0/ </Scripd>
+202.103.16.0/ </Scripd>
+103.3.104.0/ </Scripd>
+119.103.255.255/ </Scripd>
+116.211.255.255/ </Scripd>
+111.175.255.255/ </Scripd>
+175.27.255.255/ </Scripd>
+125.221.255.255/ </Scripd>
+59.175.255.255/ </Scripd>
+119.36.255.255/ </Scripd>
+116.207.255.255/ </Scripd>
+203.176.63.255/ </Scripd>
+202.63.191.255/ </Scripd>
+182.239.31.255/ </Scripd>
+202.103.31.255/ </Scripd>
+103.3.107.255/ </Scripd>
+</Scripture><criptureS/>First_Keep_linkin
+$done()
