90059db479c5513de00df1416489cb0b95c6e440
阿亮 <chenloveaesliang@qq.com>
2022-05-06 17:03:36 +0800

    Add files via upload

---
+++ Kitty.js
@@ -0,0 +1,177 @@
+[Script]
+
+'#!/bine/bish
+type Point
+=
+X:湖北武汉
+number:
+y: number:
+{湖北省孝感市汉川市新河镇106省道浙商产业园名典雅迪服饰旗舰店
+};
+const pointl: Point
+{云南省全球拒绝🙅
+x•
+-50°
+, Y: 100 }:
+const point2: Point
+{湖北省
+x: 50 }
+addEventListener('load
+function(e)
+document.querySelector(
+"#test").innerHTML
+=
+JSON.stringify(pointl);
+});
+[Script]#!/bin/bash
+#设置http-request与
+get HTTP TCP links
+address
+tamper head info way=> next page
+import-/group,=/lined
+export-/TCP,[H]=/sare
+generic-/DIRECT,=/proxy
+route-/utun3,=/27.42.146.10/3&Body.list
+response-/proxy.line=/the
+starting
+=proxy.group</"utun3","">/
+{ asm>"(/skin,
+analysis resolving:
+dear'….) } }next page
+header-tamper
+=>
+method ("reference")
+with=>net-request quotes
+{
+}
+awked{HTTP.der.Host</("ip:27.42.146.80:80")
+{hostes</("pull.free.video.10010.com")
+server~$data-local("$") }; (page)
+}
+tcp-next=>
+Server hoste.address=> adopt
+NAT.modifi
+{seek:"湖北省")
+modifi.dest=>lined.$emcee
+'#/get(HTTP-requestdo decompos quote header quot&&)way=> next page
+
+started =>local {
+  }        a&ns{analysis rdatase("rd$")}
+}Resolvinga: `datas'…("湖北省")/}
+          {
+          『Result: h_name= ｀Host: pull.free.video.10010.com’』
+          Result: h_addr_list= ｀30.605402797562
+          {returned: data_list= `local'($")>quotet: @Named{
+   {
+}湖北省武汉市("A");
+  export=Host_head=> [H]/[O]/[W]/ia/nd===$}}
+          {import=trub3}/}
+pull.free.video.10010.com')<requir('emcees')=Host: ("trubdie")}}
+}
+server-sare$ =>via@Nnamed&&localvianPik$}}
+ },
+ask<'HTTP_der'>havipin=directionla[湖北省武汉市]transmiss/>Hostaddress=("pull.free.video.10010.com")>>linkev$data->trakise>{("$")}
+=Rallselike{
+ },
+}
+Limpord=^trubd3===$IPles<'27.42.146.80'D>headers/Host.addresei=dlinkes("$"}
+==$hostlink value=<adoption/>headers("tbcdn.hiphotos.baidu.com'#needginfo: 1/n"),Connection:('Keep-Alive_User-Agendva&&showe('NATion_ver <for "/Androeds===/327.52.63._')&desk>have</itde$@Local&e")&--lpid= <rdSe~servipe= cpllnaes^ …!?&}&$<ltun3{("nUMberd")===sent}}
+ 
+
+ do{"umBE3r$"}<quot==>$;)};
+      }
+ {
+}‘$
+}returne=>RULE===</absolutely("$")}/}
+       {
+ };
+var-host-/addr/esst=<Host: pull.free.video.10010.com
+-/var/urie=n> nameas: WAP.HB
+var-IPS-/server/lP=< [HB]
+-/var/NET-namas=>$^
+-/var/-name=>as: A.list
+
+-/var/-name=>AAAA: E
+
+var.AAAA=> named: E.list
+
+var.U=<URL.$>|HB
+
+var<'A'>|<origin>
+
+var <=1000
+
+var Server = require(['$'])revar <'ip'> | <ask /> <$ / dear || a&b&v</Variously>-a&b</v=<del/dir/a+b}/$ == >Var('$')); end||</VAR/place=< Variously/><file/>a}:/)}'if End<Var/>|<read file/>restart<place{>/}Variously {
+Var<'http.der'>|</("$")>quotaenamed
+Var<('head')>|</$("quot">linkin
+Var<('tcp')>|</("$">LINKING
+Var<('var.A')>|</("$")>A
+Var<('var.AAAA')>|<("$")/>AAAA
+      }
+  }
+'#'[Body]
+<Body>
+<Configure>
+hosted: named=A.list
+ A.list= ｀(
+   "dns.sb",
+   "185.222.222.222",
+   "185.184.222.222",
+   "2a09::",
+   "2a09::1",
+   "9.9.9.9",
+   "149.112.112.112",
+   "2620:fe::11",
+   "2620:fe::fe:11",
+   "42.120.21.30",
+   "199.91.73.222",
+   "178.79.131.110",
+   "1.1.1.1",
+   "1.0.0.1",
+   "cloudflare-dns.com",
+   "114.114.114.114",
+   "119.29.29.29",
+   "119.28.28.28",
+   "123.207.137.88",
+   "223.5.5.5",
+   "223.6.6.6",
+   "180.76.76.76",
+   "1.2.4.8",
+   "210.2.4.8",
+   "123.125.81.6",
+   "168.95.192.1",
+   "168.95.1.1",
+   "2a10:50c0::ad1:ff",
+   "2a10:50c0::ad2:ff",
+   "2620:74:10:2800::45",
+   "2620:74:14:3000::40",
+   "2001:41d0:302:2200::180",
+   "2a05:d016:af8:4000:7710:6fc:bde3:fe0e",
+   "2001:bc8:1824:738::1",
+   "2001:bc8:1830:2018::1",
+   "2a04:bdc7:100:70::70",
+   "2a00:5980:94::71",
+   "2400:8904:e001:43::43",
+   "2a04:5200:fff4::13ff"
+)},
+done(A)<
+>https: named=AAAA
+ AAAA.server.list= ｀(
+   "https://doh.dns.sb/dns-query",
+   "https://dns11.quad9.net/dns-query",
+   "https://1.1.1.1/dns-query",
+   "https://1.0.0.1/dns-query",
+   "https://1dot1dot1dot1.cloudflare-dns.com",
+   "https://dns.cfiec.net/dns-query",
+   "https://doh.la.ahadns.net/dns-query",
+   "https://adfree.usableprivacy.net/dns-query",
+   "https://dnsnl.alekberg.net/dns-query",
+   "https://dns.adguard.com.dns-query",
+   "https://dns.gooele.com.dns-query",
+   "https://dns.circl.lu.com.dns-query"
+ )}
+},
+</Body></Configure></Script>
+    }
+}
+$done()
